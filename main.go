7ffa5e13b574d2ce148f1b8c3b4989cd4f081683
Pavel Druzhinin <paveldroo@gmail.com>
2024-02-06 17:20:50 +0300

    Add httprouter and CRUD methods

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	_ "github.com/lib/pq"
 	"log"
+	"net/http"
 )
 
 type Book struct {
@@ -17,20 +19,66 @@ type Book struct {
 var db *sql.DB
 var err error
 
-func main() {
+func init() {
 	db, err = sql.Open("postgres", "postgres://jonny:jonny_go@localhost:5432/bookstore?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
-
-	defer db.Close()
 	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("You successfully connected to Postgres!")
+}
 
-	rows, err := db.Query(`SELECT * FROM books;`)
+func main() {
+	r := httprouter.New()
+	r.GET("/books", getAll)
+	r.GET("/books/:id", get)
+	r.POST("/books/", create)
+	r.DELETE("/books/:id", del)
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
+
+func getAll(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	rows, err := db.Query("SELECT * FROM books;")
+	if err != nil {
+		log.Fatalln(err)
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	bks := make([]Book, 0)
+	for rows.Next() {
+		b := Book{}
+		if err := rows.Scan(&b.isbn, &b.title, &b.author, &b.price); err != nil {
+			if err != nil {
+				log.Fatalln(err)
+				http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+				return
+			}
+		}
+		bks = append(bks, b)
+	}
+
+	if err = rows.Err(); err != nil {
+		if err != nil {
+			log.Fatalln(err)
+			http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	for _, b := range bks {
+		fmt.Fprintf(w, "%s, %s, %s, $%.2f\n", b.isbn, b.title, b.author, b.price)
+	}
+}
+
+func get(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	bId := p.ByName("id")
+	rows, err := db.Query("SELECT * FROM books WHERE isbn=?", bId)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -53,5 +101,12 @@ func main() {
 	for _, b := range bks {
 		fmt.Printf("%s, %s, %s, $%.2f\n", b.isbn, b.title, b.author, b.price)
 	}
+}
+
+func create(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+
+}
+
+func del(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
 }
